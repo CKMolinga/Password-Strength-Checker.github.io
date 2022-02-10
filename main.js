de5133d9de5582f56b45d39e99b737abd5e737af
CKMolinga <ck.molinga@gmail.com>
2022-02-10 13:28:02 +0100

    Version 1.0

---
+++ main.js
@@ -0,0 +1,83 @@
+const indicator = document.querySelector(".indicator");
+const input = document.querySelector("input");
+const weak = document.querySelector(".weak");
+const medium = document.querySelector(".medium");
+const strong = document.querySelector(".strong");
+const text = document.querySelector(".text");
+const showBtn = document.querySelector(".showBtn");
+let container = document.querySelector(".container");
+let mode = document.querySelector(".mode");
+let lightMode = true;
+let regExpWeak = /[a-z]/;
+let regExpMedium = /\d+/;
+let regExpStrong = /.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/;
+
+function toggleMode() {
+    lightMode = !lightMode;
+    console.log(lightMode);
+    if (lightMode == false) {
+        document.body.style.backgroundColor = "#18141e";
+        container.style.backgroundColor = "#5d5c67";
+        document.body.style.color = "#fff";
+        mode.textContent = "LightMode"
+    } else {
+        document.body.style.backgroundColor = "#f2f2f2";
+        container.style.backgroundColor = "#fff";
+        document.body.style.color = "#000";
+        mode.textContent = "DarkMode"
+    }
+
+}
+
+function trigger() {
+    if (input.value != "") {
+        indicator.style.display = "block";
+        indicator.style.display = "flex";
+        if (input.value.length <= 3 && (input.value.match(regExpWeak) || input.value.match(regExpMedium) || input.value.match(regExpStrong))) no = 1;
+        if (input.value.length >= 6 && ((input.value.match(regExpWeak) && input.value.match(regExpMedium)) || (input.value.match(regExpMedium) && input.value.match(regExpStrong)) || (input.value.match(regExpWeak) && input.value.match(regExpStrong)))) no = 2;
+        if (input.value.length >= 6 && input.value.match(regExpWeak) && input.value.match(regExpMedium) && input.value.match(regExpStrong)) no = 3;
+        if (no == 1) {
+            weak.classList.add("active");
+            text.style.display = "block";
+            text.textContent = "Your password is too week";
+            text.classList.add("weak");
+            input.style.borderColor = "#ff4757";
+        }
+        if (no == 2) {
+            medium.classList.add("active");
+            text.textContent = "Your password is medium";
+            text.classList.add("medium");
+            input.style.borderColor = "orange";
+        } else {
+            medium.classList.remove("active");
+            text.classList.remove("medium");
+        }
+        if (no == 3) {
+            weak.classList.add("active");
+            medium.classList.add("active");
+            strong.classList.add("active");
+            text.textContent = "Your password is strong";
+            text.classList.add("strong");
+            input.style.borderColor = "#23ad5c";
+        } else {
+            strong.classList.remove("active");
+            text.classList.remove("strong");
+        }
+        showBtn.style.display = "block";
+        showBtn.onclick = function() {
+            if (input.type == "password") {
+                input.type = "text";
+                showBtn.textContent = "HIDE";
+                showBtn.style.color = "#23ad5c";
+            } else {
+                input.type = "password";
+                showBtn.textContent = "SHOW";
+                showBtn.style.color = "#000";
+            }
+        }
+    } else {
+        indicator.style.display = "none";
+        text.style.display = "none";
+        showBtn.style.display = "none";
+    }
+}
